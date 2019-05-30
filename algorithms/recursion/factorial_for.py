a3cb2903ee9d7f2be208f9213963477c68173fb8
woonhahaha <woonhahaha@gmail.com>
2019-05-31 00:55:38 +0900

    recursion_factorial

---
+++ algorithms/recursion/factorial_for.py
@@ -0,0 +1,11 @@
+def factorial(n):
+	result = 1
+	for i in range(1, n+1):
+		result *= i
+	return result
+
+if __name__ == '__main__':
+	print("The value of 5! should be ", 5*4*3*2*1)
+	print("The value of 5! is ", factorial(5))
+	print("The value of 0! should be 1")
+	print("The value of 0! is ", factorial(0))
---
+++ algorithms/recursion/factorial_recursive.py
@@ -0,0 +1,10 @@
+def factorial(n):
+    if n == 0:
+        return 1
+    return n * factorial(n - 1)
+
+if __name__ == '__main__':
+	print("The value of 5! should be ", 5*4*3*2*1)
+	print("The value of 5! is ", factorial(5))
+	print("The value of 0! should be 1")
+	print("The value of 0! is ", factorial(0))
