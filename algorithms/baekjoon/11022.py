9369566cad1f453c8c11a260ed1446d80a8d606b
woonhahaha <woonhahaha@gmail.com>
2019-08-20 22:25:51 +0800

    baekjoon #11022

---
+++ algorithms/baekjoon/11022.py
@@ -0,0 +1,7 @@
+num = int(input())
+arr = []
+for i in range(num):
+    arr.append(list(map(int, input().split())))
+
+for i in range(num):
+    print("Case #%d: %d + %d = %d" % (i+1, arr[i][0], arr[i][1], arr[i][0] + arr[i][1]))
